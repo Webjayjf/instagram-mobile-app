6e5b4b2732687212ea969031096ffdf0945e125c
mucahit-sahin <mucahitsahin786@gmail.com>
2023-03-11 17:44:47 +0300

    Added listing photos from media library on the Add Screen

--- src/Screens/Add/index.js
+++ src/Screens/Add/index.js
@@ -1,12 +1,103 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  Image,
+  FlatList,
+} from "react-native";
+import React, { useEffect } from "react";
+import { AntDesign } from "@expo/vector-icons";
+import * as MediaLibrary from "expo-media-library";
+import * as ImagePicker from "expo-image-picker";
 
 import styles from "./style";
 
-const Add = () => {
+const Add = ({ navigation }) => {
+  // hide tabBar
+  navigation.setOptions({
+    tabBarStyle: { display: "none" },
+  });
+
+  const [hasGalleryPermission, setHasGalleryPermission] = React.useState(null);
+  const [userGalleryMedia, setUserGalleryMedia] = React.useState(null);
+
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const galleryStatus =
+          await ImagePicker.requestMediaLibraryPermissionsAsync();
+        setHasGalleryPermission(galleryStatus.status === "granted");
+        if (galleryStatus.status == "granted") {
+          const response = await MediaLibrary.getAssetsAsync({
+            mediaType: "photo",
+            sortType: "newest",
+          });
+          setUserGalleryMedia(response.assets);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (userGalleryMedia) {
+      setSelectedImage(userGalleryMedia[0]);
+    }
+  }, [userGalleryMedia]);
+
   return (
-    <View>
-      <Text>Add</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity
+          style={styles.backButton}
+          onPress={() => navigation.navigate("Home")}
+        >
+          <AntDesign name="close" size={24} color="black" />
+        </TouchableOpacity>
+        <Text style={styles.title}>New Post</Text>
+        <TouchableOpacity style={styles.nextButton}>
+          <AntDesign name="arrowright" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.content}>
+        {selectedImage && (
+          <FlatList
+            style={styles.mediaList}
+            data={userGalleryMedia}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                style={{
+                  opacity: item == selectedImage ? 0.5 : 1,
+                }}
+                onPress={() => {
+                  setSelectedImage(item);
+                }}
+              >
+                <Image style={styles.image} source={{ uri: item.uri }} />
+              </TouchableOpacity>
+            )}
+            keyExtractor={(item, index) => index}
+            numColumns={3}
+            ListHeaderComponent={() => (
+              <Image
+                style={styles.bigImage}
+                source={{
+                  uri: selectedImage.uri,
+                }}
+              />
+            )}
+            removeClippedSubviews
+            maxToRenderPerBatch={3}
+            initialNumToRender={3}
+            windowSize={3}
+          />
+        )}
+        {/* */}
+      </View>
     </View>
   );
 };
--- src/Screens/Add/style.js
+++ src/Screens/Add/style.js
@@ -1,5 +1,52 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
-const styles = StyleSheet.create({});
+import { colors } from "../../Theme/colors";
+
+const { width, height } = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.borderColor,
+  },
+  backButton: {},
+  backButtonText: {
+    color: colors.primaryTextColor,
+    fontSize: 20,
+  },
+  title: {
+    color: colors.primaryTextColor,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  nextButton: {},
+  nextButtonText: {
+    color: colors.primaryTextColor,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  content: {
+    flex: 1,
+  },
+  bigImage: {
+    width: width,
+    height: width,
+  },
+  mediaList: {
+    flex: 1,
+  },
+  image: {
+    width: width / 3,
+    height: width / 3,
+  },
+});
 
 export default styles;
